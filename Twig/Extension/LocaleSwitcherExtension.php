d720001c3f47c800bf6e82992ac2603f401f102e
Tomáš Bláha <tomas.blaha@outlook.com>
2023-07-12 08:14:08 +0200

    Fix

--- Twig/Extension/LocaleSwitcherExtension.php
+++ Twig/Extension/LocaleSwitcherExtension.php
@@ -13,12 +13,13 @@ namespace Lunetics\LocaleBundle\Twig\Extension;
 
 use Lunetics\LocaleBundle\Templating\Helper\LocaleSwitchHelper;
 use Lunetics\LocaleBundle\Switcher\TargetInformationBuilder;
+use Twig\Extension\AbstractExtension;
 
 /**
  * @author Christophe Willemsen <willemsen.christophe@gmail.com>
  * @author Matthias Breddin <mb@lunetics.com>
  */
-class LocaleSwitcherExtension extends \Twig_Extension
+class LocaleSwitcherExtension extends AbstractExtension
 {
     /**
      * @var TargetInformationBuilder
